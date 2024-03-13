cbe963ba9f36022f2376f95278d25c92ea3e2c7c
vishnuteja0201 <134588072+vishnuteja0201@users.noreply.github.com>
2024-03-13 16:41:36 +0530

    SwapNo.java

--- SwapNo.java
+++ SwapNo.java
@@ -1,24 +1,15 @@
-//Question 2. Write a java program to swap two numbers without using a third variable and (+ and -) operators? 
-//int a = 200, b = 300;
+int a = 200, b = 300;
+public class Main {
+    public static void main(String[] args) {
+        int a = 200, b = 300;
+        System.out.println("Before swapping - a: " + a + ", b: " + b);
+        swapNumbersWithoutTempVariable(a, b);
+    }
 
-import java.util.*;
-public class SwapNo 
-{
-	public static void main(String[] args)
-	{
-		int a=200;
-		int b=300;
-		
-		System.out.println("Before swapping:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        
-        a = a * b; 
-        b = a / b;  
-        a = a / b;  
-        
-        System.out.println("\nAfter swapping:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-	}
+    public static void swapNumbersWithoutTempVariable(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping - a: " + a + ", b: " + b);
+    }
 }
